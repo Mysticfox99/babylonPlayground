06b8857c3cb46a600bd19da145dc9e73c11c5bba
Ernestas <ernestas.milinskas@gmail.com>
2024-09-19 23:11:02 +0100

    Update vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "https://www.vintgram.com/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/babylonPlayground/" : "/",
 };
